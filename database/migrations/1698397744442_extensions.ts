887b57758fa388c7c155adc352861435a34a2bf5
agustinalonsocantoli <agustinalonsocantoli@gmail.com>
2023-11-09 09:27:58 +0100

    cambios en migraciones

--- database/migrations/1698397744442_extensions.ts
+++ database/migrations/1698397744442_extensions.ts
@@ -2,8 +2,10 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class Extensions extends BaseSchema {
   public async up() {
-    this.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    this.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp" schema pg_catalog version "1.1";')
   }
 
-  public async down() {}
+  public async down() {
+    this.schema.raw('DROP EXTENSION IF EXISTS "uuid-ossp"')
+  }
 }
